70f4e0ac0ab6984690fbe3d0d1dc0f284973ab83
jgiboney <jgiboney@gmail.com>
2026-05-06 11:38:34 -0600

    day 3 in class

---
+++ day 3/fastfood.py
@@ -0,0 +1,11 @@
+name = "Justin"
+food = ""
+
+if food and name:
+    form_complete = True
+
+if form_complete:
+    print(f"{name}'s favorite food is {food}")
+else:
+    print("Please fill out the form completely.")
+    
