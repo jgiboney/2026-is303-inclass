1cd4a33e4de1f1e8975e83ad360dccc1d1817f49
jgiboney <jgiboney@gmail.com>
2026-06-10 15:08:15 -0600

    Day 12 material

--- day 12/car_look_2.py
+++ day 12/car_look_2.py
@@ -1,7 +1,22 @@
 from bs4 import BeautifulSoup
+from peewee import *
 import requests, time
 
+db = SqliteDatabase("vehicles.db")
 
+class Vehicle(Model):
+    make = CharField()
+    model = CharField()
+    year = IntegerField()
+    mpg = IntegerField()
+    category = CharField()
+    tco_10yr = IntegerField()
+
+    class Meta:
+        database = db
+
+db.connect()
+db.create_tables([Vehicle])
 
 def get_fuel_data (year, make, model):
     """
@@ -29,7 +44,6 @@ def get_fuel_data (year, make, model):
     base_headers = {"Accept":"application/json"}
     url = base_url + f"vehicle/menu/options?year={year}&make={make}&model={model}"
     response = requests.get(url, headers=base_headers)
-    print(response.text)
     data = response.json()
     if type(data["menuItem"]) == list:
         vehicle_id = data["menuItem"][0]["value"]
@@ -41,7 +55,6 @@ def get_fuel_data (year, make, model):
     response = requests.get(url, headers=base_headers)
     data = response.json()
     return int(data["comb08"])
-
 
 def get_maintenance_cost (make, model):
     """
@@ -76,12 +89,25 @@ def get_maintenance_cost (make, model):
         total_maintenance += int_amount
     return total_maintenance
 
+def store_vehicle(data):
+    existing = Vehicle.get_or_none(
+        (Vehicle.make == data["make"]) &
+        (Vehicle.model == data["model"]) &
+        (Vehicle.year == data["year"])
+    )
+    if existing:
+        print(f"Skipping {data['make']} {data['model']}")
+        return
+
+    Vehicle.create(**data)
+    print(f"Stored {data['make']} {data['model']}")
+
 
 list_of_vehicles = [
-    {"year":2020, "make": "Honda", "model": "Civic", "extra_text": " 4Dr"},
-    {"year":2020, "make": "Chevrolet", "model": "Blazer", "extra_text": " AWD"},
-    {"year":2023, "make": "Toyota", "model": "Corolla", "extra_text": ""},
-    {"year":2026, "make": "Toyota", "model": "Prius", "extra_text": " AWD"}
+    {"year":2020, "make": "Honda", "model": "Civic", "extra_text": " 4Dr", "category":"sedan"},
+    {"year":2020, "make": "Chevrolet", "model": "Blazer", "extra_text": " AWD", "category":"suv"},
+    {"year":2023, "make": "Toyota", "model": "Corolla", "extra_text": "", "category":"sedan"},
+    {"year":2026, "make": "Toyota", "model": "Prius", "extra_text": "", "category":"sedan"}
 ]
 
 for vehicle in list_of_vehicles:
@@ -93,3 +119,19 @@ for vehicle in list_of_vehicles:
     print(f"{vehicle["year"]} {vehicle["make"]} {vehicle["model"]} TCO: {tco}")
     time.sleep(5)
 
+    vehicle_data = {"make":vehicle["make"], "model":vehicle["model"],
+            "year":vehicle["year"], "mpg":mpg,
+            "category":vehicle["category"], "tco_10yr":tco}
+    store_vehicle(vehicle_data)
+
+
+
+
+
+
+
+
+
+
+for v in Vehicle.select():
+    print(v.year, v.make, v.model, v.tco_10yr)
